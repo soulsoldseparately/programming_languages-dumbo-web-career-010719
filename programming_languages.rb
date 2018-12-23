6bb663bfbbaa6223221a73ac146e21a0f9110890
Daniel Toma <danieltoma@ubalumni.com>
2018-12-23 02:02:04 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,17 +1,17 @@
 def reformat_languages(languages)
- 
-  new_hash = {}
-  
-  languages.each do |style, value|
-    value.each do |k, v|
-      v.each do |key, val|
-        
-        if new_hash.has_key?(k)
-          new_hash[k][:style] << style
+ new_hash = {}
+
+  languages.each do |style, language_hash|
+   # puts language_hash
+    language_hash.each do |language, type|
+      type.each do |key, val|
+
+        if new_hash.has_key?(language)
+          new_hash[language][:style] << style
         else
-          new_hash[k] = {}
-          new_hash[k][:type] = val
-          new_hash[k][:style] = [] << style
+          new_hash[language] = {}
+          new_hash[language][:type] = val
+          new_hash[language][:style] = [] << style
         end
       end
     end
@@ -19,4 +19,21 @@ def reformat_languages(languages)
   new_hash
 end
 
-#Took me a good long while to get this working!
+#3-------REFACTORED with 2 instances of .each instead of 3------#
+
+def reformat_languages(languages)
+ new_hash = {}
+
+  languages.each do |style, language_hash|
+    language_hash.each do |language, type|
+      if new_hash.has_key?(language)
+        new_hash[language][:style] << style
+      else
+        new_hash[language] = {}
+        new_hash[language][:type] = type.values[0]
+        new_hash[language][:style] = [] << style
+      end
+    end
+  end
+  new_hash
+end
